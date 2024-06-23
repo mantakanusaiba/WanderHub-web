e6cc8a961ccd272abdff38292fd8f2289f32fd9d
Atandrila <138500994+Atandrila@users.noreply.github.com>
2024-06-23 11:50:43 +0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,29 @@
-// src/components/Header.js
 import React from 'react';
+import logo from './img/logo.png'; 
 
 const Header = ({ onNavClick }) => {
   return (
     <header>
       <div className="top-bar">
+        <div className="logo-container">
+          <img src={logo} alt="WanderHub Logo" className="logo" />
+        </div>
         <h1>WanderHub</h1>
         <div className="profile">
           <button id="loginButton">Login</button>
         </div>
       </div>
-      <nav>
-        <ul>
-          <li><button onClick={() => onNavClick('home')}>Home</button></li>
+      <nav className="nav-bar">
+        <ul className="nav-left">
+        <li><button onClick={() => onNavClick('home')}>Home</button></li>
           <li><button onClick={() => onNavClick('holiday')}>Holiday</button></li>
+          <li><button onClick={() => onNavClick('hotel')}>Hotel</button></li>
           <li className="dropdown">
-            <button>Others</button>
+            <a href="#">Others</a>
             <div className="dropdown-content">
-              <button onClick={() => onNavClick('why')}>Why WanderHub?</button>
-              <button onClick={() => onNavClick('faq')}>FAQ</button>
-              <button onClick={() => onNavClick('about')}>About</button>
+              <a href="#">Why WanderHub?</a>
+              <a href="#">FAQ</a>
+              <a href="#">About</a>
             </div>
           </li>
         </ul>
