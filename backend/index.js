acb7ef190b6094f28ffb83e15c91d5635ed79a5b
mantakanusaiba <123790428+mantakanusaiba@users.noreply.github.com>
2024-07-07 12:35:56 +0600

    Update and rename server.js to index.js

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const dotenv = require('dotenv');
+
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+
+
+dotenv.config();
+
+console.log(MONGO_URI: ${process.env.MONGO_URI}); // Log MONGO_URI
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+// Root route
+app.get("/", (req, res) => {
+  res.send('Server is running');
+});
+
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(Server running on port ${PORT}));
