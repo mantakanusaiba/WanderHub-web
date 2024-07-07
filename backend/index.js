c463fbd1441553d3c3cdc5308221d56d871ce7ca
mantakanusaiba <123790428+mantakanusaiba@users.noreply.github.com>
2024-07-07 12:41:37 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,13 @@ console.log(MONGO_URI: ${process.env.MONGO_URI}); // Log MONGO_URI
 
 connectDB();
 
-app.use(cors());
+app.use(cors(
+    {
+      origin:[""],
+      methods: ["POST","GET"],
+      credentials: true
+    }
+));
 app.use(express.json());
 
 // Root route
